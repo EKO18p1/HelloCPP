84ff596798039c988014ae39cf09222211ae0b0a
Test <eko18p1@gmail.com>
2018-11-15 09:55:57 +0200

    Add project files.

---
+++ HelloCPP/ResoucesStrings.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stdafx.h"
+
+const string cinUserName = "Enter name";
+const string helloString = "Hello me";
+const string menu[3]{ "Hello", "Exit", "Settings" };
+const string menuChooser = "-->";
